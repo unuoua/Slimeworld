faf296540d2e6683ae02551d12073e97e542010b
elliot <elliot.itsu@gmail.com>
2017-10-21 16:15:58 -0500

    changed some stuff to see if routing works

--- SLIMEWORLD/urls.py
+++ SLIMEWORLD/urls.py
@@ -17,8 +17,10 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from django.views.generic import TemplateView
+from .views import HomeView, home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^genre/$', TemplateView.as_view(template_name='genre.html'), name='genre'),
+    url(r'^genre/$', TemplateView.as_view(template_name='genre.html')),
+    url(r'^e/$', home),
 ]
---
+++ SLIMEWORLD/views.py
@@ -0,0 +1,30 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse("Hello")
+
+
+class HomeView(TemplateView):
+    template_name = 'genre.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = None
+        some_list = [
+            random.randint(0, 100000000),
+            random.randint(0, 100000000),
+            random.randint(0, 100000000)
+        ]
+        condition_bool_item = True
+        if condition_bool_item:
+            num = random.randint(0, 100000000)
+        context = {
+            "num": num,
+            "some_list": some_list
+        }
+        return context
